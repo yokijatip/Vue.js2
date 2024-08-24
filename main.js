fefbc6b5f571d9ce82aeddf0dde5fa4cff75649e
Yoki Jati Perkasa <yokijati@gmail.com>
2024-08-24 11:47:08 +0700

    Membuat perulangan di dalam Vue.js

---
+++ main.js
@@ -0,0 +1,90 @@
+var dataDestination = {
+  destinations: [
+    {
+      id: 1,
+      name: "Bali",
+      image:
+        "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, impedit.",
+      place: "Indonesia",
+    },
+    {
+      id: 2,
+      name: "Malang",
+      image:
+        "https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, impedit.",
+      place: "Indonesia",
+    },
+    {
+      id: 3,
+      name: "Jakarta",
+      image:
+        "https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, impedit.",
+      place: "Indonesia",
+    },
+    {
+      id: 4,
+      name: "Surabaya",
+      image:
+        "https://images.unsplash.com/photo-1552832230-c0197dd311b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, impedit.",
+      place: "Indonesia",
+    },
+    {
+      id: 5,
+      name: "Bandung",
+      image:
+        "https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, impedit.",
+      place: "Indonesia",
+    },
+    {
+      id: 6,
+      name: "Yogyakarta",
+      image:
+        "https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, impedit.",
+      place: "Indonesia",
+    },
+    {
+      id: 7,
+      name: "Semarang",
+      image:
+        "https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, impedit.",
+      place: "Indonesia",
+    },
+    {
+      id: 8,
+      name: "Lombok",
+      image:
+        "https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, impedit.",
+      place: "Indonesia",
+    },
+    {
+      id: 9,
+      name: "Bali",
+      image:
+        "https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, impedit.",
+      place: "Indonesia",
+    },
+  ],
+};
+
+var app = new Vue({
+  el: "#app",
+  data: dataDestination,
+});
